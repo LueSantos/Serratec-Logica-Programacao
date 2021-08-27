16988fdcb295e401df755347061c8d511fd73342
LueSantos <86605122+LueSantos@users.noreply.github.com>
2021-08-27 12:06:25 -0300

    arquivo errado

--- faseCinco/dissertativos.js
+++
@@ -1,9 +0,0 @@
-/*Dissertativos
-Qual o único valor avaliado por um seletor ‘se então’?
-Onde termina uma instrução if?
-O que podemos fazer para que a manutenção de um condicional seja mais simples, ou mais legível, e ainda ajudar na depuração?
-Como podemos estender o seletor if, para que sempre uma instrução seja executada (um desvio)?
-É possível que mais de uma instrução seja executada por um condicional?
-É possível criar uma atribuição com desvio sem utilizar o senão?
-Pra quê o seletor deve conter blocos?
- */
